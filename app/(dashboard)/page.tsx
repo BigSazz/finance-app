34226379723f2de53a2d517aad826307ad5c33e3
Osaze Oviawe <ocubed88@gmail.com>
2024-06-03 17:10:37 +0100

    - ui update

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,11 +1,15 @@
+"use client";
 import { UserButton } from "@clerk/nextjs";
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
+  const { onOpen } = useNewAccount();
   return (
     <div>
+      <Button onClick={onOpen}>Open New Account</Button>
       <h1>Dashboard Page</h1>
       <p>This a protected page.</p>
-      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
