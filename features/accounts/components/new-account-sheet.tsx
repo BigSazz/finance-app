28221c24a583d9024376080c321bfea1848c9b82
Osaze Oviawe <ocubed88@gmail.com>
2024-06-04 14:19:29 +0100

    - add functionality for creating account and saving to db

--- features/accounts/components/new-account-sheet.tsx
+++ features/accounts/components/new-account-sheet.tsx
@@ -1,4 +1,11 @@
+import { z } from "zod";
+
 import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+import { AccountForm } from "@/features/accounts/components/account-form";
+import { useCreateAccount } from "@/features/accounts/api/use-create-account";
+
+import { insertAccountSchema } from "@/db/schema";
+
 import {
   Sheet,
   SheetContent,
@@ -7,8 +14,25 @@ import {
   SheetTitle,
 } from "@/components/ui/sheet";
 
+const formSchema = insertAccountSchema.pick({
+  name: true,
+});
+
+type FormValues = z.infer<typeof formSchema>;
+
 export const NewAccountSheet = () => {
   const { isOpen, onClose } = useNewAccount();
+
+  const mutation = useCreateAccount();
+
+  const onSubmit = (values: FormValues) => {
+    mutation.mutate(values, {
+      onSuccess: () => {
+        onClose();
+      },
+    });
+  };
+
   return (
     <Sheet open={isOpen} onOpenChange={onClose}>
       <SheetContent className="space-y-4">
@@ -18,6 +42,7 @@ export const NewAccountSheet = () => {
             Create a new account to track your transactions
           </SheetDescription>
         </SheetHeader>
+        <AccountForm onSubmit={onSubmit} disabled={mutation.isPending} />
       </SheetContent>
     </Sheet>
   );
