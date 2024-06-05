b0af4f65bdd7c230e95af1941fa429840d43952b
Osaze Oviawe <ocubed88@gmail.com>
2024-06-05 15:36:48 +0100

    - Add endpoint to fetch a single account by ID - Implement account update functionality

---
+++ app/(dashboard)/accounts/actions.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { Edit, MoreHorizontal } from "lucide-react";
+import { useOpenAccount } from "@/features/accounts/hooks/use-open-account";
+
+type ActionsProps = {
+  id: string;
+};
+
+export const Actions = ({ id }: ActionsProps) => {
+  const { onOpen } = useOpenAccount();
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="size-8 p-0">
+            <MoreHorizontal className="size-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem disabled={false} onClick={() => onOpen(id)}>
+            <Edit className="mr-2 size-4" />
+            Edit
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+};
--- app/(dashboard)/accounts/columns.tsx
+++ app/(dashboard)/accounts/columns.tsx
@@ -7,6 +7,7 @@ import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 import { InferResponseType } from "hono";
 import { client } from "@/lib/hono";
+import { Actions } from "@/app/(dashboard)/accounts/actions";
 
 export type ResponseType = InferResponseType<
   typeof client.api.accounts.$get,
@@ -49,5 +50,9 @@ export const columns: ColumnDef<ResponseType>[] = [
         </Button>
       );
     },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => <Actions id={row.original.id} />,
   },
 ];
